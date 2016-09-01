759204dab69e93ea23fa635ef4bb5b62cce2cb86
Ajay Gandhi <aag255@cornell.edu>
2016-09-01 13:27:50 -0400

    Write up postgres

--- postgres.js
+++ postgres.js
@@ -15,170 +15,164 @@ module.exports = (function () {
   function PostGres () {
     this.users = {};
     var self = this;
-    // fs.readFile(path, function (err, data) {
-    //   if (err && err.code !== 'ENOENT') console.error(err);
-    //   if (data) self.users = JSON.parse(data);
-    // });
 
     pg.connect(process.env.DATABASE_URL, function (err, client) {
       if (err) throw err;
 
-      // client
-      //   .query('SELECT table_schema,table_name FROM information_schema.tables;')
-      //   .on('row', function(row) {
-      //     console.log(JSON.stringify(row));
-      //   });
+      self.client = client;
 
-      client
-        .query('CREATE TABLE IF NOT EXISTS users (' +
-          'user_id SERIAL PRIMARY KEY, ' +
-          'fb_user_id varchar(20) NOT NULL, ' +
-          'data text NOT NULL' +
-        ')');
-
-      console.log('table exists');
-
-      client
-        .query('INSERT INTO users (fb_user_id, data) ' +
-          "VALUES ('1', 'this is the data')");
+      client.query('CREATE TABLE IF NOT EXISTS users (' +
+        'user_id SERIAL PRIMARY KEY, ' +
+        'fb_user_id varchar(20) NOT NULL, ' +
+        'data text NOT NULL' +
+      ')');
 
       client
         .query('SELECT * FROM users')
-        .on('row', function (row) {
-          console.log(row);
-          this.users[row['user_id']] = row['data'];
-        });
+        .on('row', this.save_user);
     });
   }
 
   /**
    * Get a user from the database. If the user isn't found, create it.
    */
-  // PostGres.prototype.get_user = function (uid) {
-  //   var user = this.users[uid];
-  //   if (user) return user;
+  PostGres.prototype.get_user = function (fb_uid) {
+    var user = this.users[fb_uid];
+    if (user) return user;
 
-  //   // User didn't exist, create new
-  //   this.save_user({
-  //     user_id: uid,
-  //     roms: {}
-  //   });
-  //   return this.users[uid];
-  // }
+    // User didn't exist, create new
+    this.save_user({
+      fb_user_id: fb_uid,
+      roms: {}
+    });
+    return this.users[fb_uid];
+  }
 
-  // /**
-  //  * Updates a user in the database
-  //  */
-  // PostGres.prototype.save_user = function (user_data) {
-  //   this.users[user_data.user_id] = user_data;
-  // }
+  /**
+   * Updates a user in the database
+   */
+  PostGres.prototype.save_user = function (user_data) {
+    this.users[user_data.fb_user_id] = user_data;
+  }
 
-  // /****************************** Public Methods ******************************/
+  /****************************** Public Methods ******************************/
 
-  // /**
-  //  * Add the ROM to the user's list of ROMs
-  //  */
-  // PostGres.prototype.add_new_rom = function (uid, rom_name, rom_url) {
-  //   var user = this.get_user(uid);
-  //   if (!user.roms[rom_name] && Object.keys(user.roms).length > MAX_ROMS)
-  //     return 'Limit of ' + MAX_ROMS + ' ROMs reached.';
+  /**
+   * Add the ROM to the user's list of ROMs
+   */
+  PostGres.prototype.add_new_rom = function (fb_uid, rom_name, rom_url) {
+    var user = this.get_user(fb_uid);
+    if (!user.roms[rom_name] && Object.keys(user.roms).length > MAX_ROMS)
+      return 'Limit of ' + MAX_ROMS + ' ROMs reached.';
 
-  //   user.roms[rom_name] = {};
-  //   user.roms[rom_name].url = rom_url;
-  //   user.roms[rom_name].saves = {};
-  //   this.save_user(user);
-  //   return true;
-  // }
+    user.roms[rom_name] = {};
+    user.roms[rom_name].url = rom_url;
+    user.roms[rom_name].saves = {};
+    this.save_user(user);
+    return true;
+  }
 
-  // /**
-  //  * Add the save file to the user's list of saves. Returns false if the rom
-  //  * wasn't found, else true.
-  //  */
-  // PostGres.prototype.update_save = function (uid, rom_name, save_name, save_data) {
-  //   var user = this.get_user(uid);
-  //   if (user.roms[rom_name]) {
-  //     if (!user.roms[rom_name].saves[save_name] && Object.keys(user.roms[rom_name].saves).length > MAX_SAVES)
-  //       return 'Limit of ' + MAX_ROMS + ' saves reached.';
+  /**
+   * Add the save file to the user's list of saves. Returns false if the rom
+   * wasn't found, else true.
+   */
+  PostGres.prototype.update_save = function (fb_uid, rom_name, save_name, save_data) {
+    var user = this.get_user(fb_uid);
+    if (user.roms[rom_name]) {
+      if (!user.roms[rom_name].saves[save_name] && Object.keys(user.roms[rom_name].saves).length > MAX_SAVES)
+        return 'Limit of ' + MAX_ROMS + ' saves reached.';
 
-  //     user.roms[rom_name].saves[save_name] = save_data;
-  //     this.save_user(user);
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
+      user.roms[rom_name].saves[save_name] = save_data;
+      this.save_user(user);
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-  // /**
-  //  * Get a list of savegames for the given user for the given game. Returns
-  //  * false if game not found, and empty array if no saves exist.
-  //  */
-  // PostGres.prototype.get_saves = function (uid, rom_name) {
-  //   var user = this.get_user(uid);
-  //   return user.roms[rom_name] ? Object.keys(user.roms[rom_name].saves) : false;
-  // }
+  /**
+   * Get a list of savegames for the given user for the given game. Returns
+   * false if game not found, and empty array if no saves exist.
+   */
+  PostGres.prototype.get_saves = function (fb_uid, rom_name) {
+    var user = this.get_user(fb_uid);
+    return user.roms[rom_name] ? Object.keys(user.roms[rom_name].saves) : false;
+  }
 
-  // /**
-  //  * Gets savedata for the given save name, rom name, user. Returns false if a
-  //  * save with the given name doesn't exist, etc.
-  //  */
-  // PostGres.prototype.get_save_data = function (uid, rom_name, save_name) {
-  //   var user = this.get_user(uid);
-  //   if (user.roms[rom_name] && user.roms[rom_name].saves[save_name])
-  //     return user.roms[rom_name].saves[save_name];
-  //   else
-  //     return false;
-  // }
+  /**
+   * Gets savedata for the given save name, rom name, user. Returns false if a
+   * save with the given name doesn't exist, etc.
+   */
+  PostGres.prototype.get_save_data = function (fb_uid, rom_name, save_name) {
+    var user = this.get_user(fb_uid);
+    if (user.roms[rom_name] && user.roms[rom_name].saves[save_name])
+      return user.roms[rom_name].saves[save_name];
+    else
+      return false;
+  }
 
-  // /**
-  //  * Gets the URl for the given rom name for the given user. Returns false if
-  //  * the ROM isn't found
-  //  */
-  // PostGres.prototype.get_rom_url = function (uid, rom_name) {
-  //   var user = this.get_user(uid);
-  //   return user.roms[rom_name] ? user.roms[rom_name].url : false;
-  // }
+  /**
+   * Gets the URl for the given rom name for the given user. Returns false if
+   * the ROM isn't found
+   */
+  PostGres.prototype.get_rom_url = function (fb_uid, rom_name) {
+    var user = this.get_user(fb_uid);
+    return user.roms[rom_name] ? user.roms[rom_name].url : false;
+  }
 
-  // /**
-  //  * Get a list of roms for the given user. Returns empty array if no roms
-  //  * exist.
-  //  */
-  // PostGres.prototype.get_rom_list = function (uid) {
-  //   var user = this.get_user(uid);
-  //   return Object.keys(user.roms);
-  // }
+  /**
+   * Get a list of roms for the given user. Returns empty array if no roms
+   * exist.
+   */
+  PostGres.prototype.get_rom_list = function (fb_uid) {
+    var user = this.get_user(fb_uid);
+    return Object.keys(user.roms);
+  }
 
-  // /**
-  //  * Delete a ROM. Returns false if ROM didn't exist
-  //  */
-  // PostGres.prototype.delete_rom = function (uid, rom_name) {
-  //   var user = this.get_user(uid);
-  //   var deleted = user.roms[rom_name] ? delete user.roms[rom_name] : false;
-  //   this.save_user(user);
-  //   return deleted;
-  // }
+  /**
+   * Delete a ROM. Returns false if ROM didn't exist
+   */
+  PostGres.prototype.delete_rom = function (fb_uid, rom_name) {
+    var user = this.get_user(fb_uid);
+    var deleted = user.roms[rom_name] ? delete user.roms[rom_name] : false;
+    this.save_user(user);
+    return deleted;
+  }
 
-  // /**
-  //  * Delete a save. Returns false if the save didn't exist
-  //  */
-  // PostGres.prototype.delete_save = function (uid, rom_name, save_name) {
-  //   var user = this.get_user(uid);
-  //   if (user.roms[rom_name] && user.roms[rom_name].saves[save_name]) {
-  //     delete user.roms[rom_name].saves[save_name];
-  //     this.save_user(user);
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
+  /**
+   * Delete a save. Returns false if the save didn't exist
+   */
+  PostGres.prototype.delete_save = function (fb_uid, rom_name, save_name) {
+    var user = this.get_user(fb_uid);
+    if (user.roms[rom_name] && user.roms[rom_name].saves[save_name]) {
+      delete user.roms[rom_name].saves[save_name];
+      this.save_user(user);
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-  // /**
-  //  * Page the cache to disk.
-  //  */
-  // PostGres.prototype.page = function () {
-  //   fs.writeFile(this.path, JSON.stringify(this.users), function (err) {
-  //     if (err) console.error('Error paging user database to disk:', err);
-  //   });
-  // }
+  /**
+   * Page the user to the database.
+   */
+  PostGres.prototype.page = function (fb_uid) {
+    var user = this.get_user(fb_uid);
+
+    if (user.user_id) {
+      // Update
+      this.client.query("UPDATE users SET data = '" + JSON.stringify(user.roms) +
+        "' WHERE user_id = '" + user.user_id + "'");
+
+    } else {
+      // New addition to db
+      this.client
+        .query('INSERT INTO users (fb_user_id, data) ' +
+          "VALUES ('" + fb_uid + "', '" + JSON.stringify(user.roms) + "') " +
+          'RETURNING user_id, fb_user_id, data')
+        .on('row', this.save_user);
+    }
+  }
 
   return PostGres;
 
